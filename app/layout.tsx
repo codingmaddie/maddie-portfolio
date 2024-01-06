dd8950feb83eb08543df307ebe9791e02d99f243
codingmaddie <madelinectang@gmail.com>
2024-01-05 21:49:47 -0500

    Fixed repeat issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { Outfit } from 'next/font/google';
-import Navbar from '../components/navbar';
-import Footer from '../components/footer';
 import './globals.css';
-
-
-import HomeBackground from '../components/homebackground';
-import Gallery from '../components/gallery';
-import Skills from "../components/skills";
-import Projects from "../components/projects";
 const outfit = Outfit({ subsets: ['latin'] });
 
 export default function RootLayout({
@@ -18,19 +10,10 @@ export default function RootLayout({
 }){
   return (
     <html>
-      <main>
-        <HomeBackground />
-        <Skills />
-        <Projects />
-        <Gallery />
+      <main>      
         {children}
       </main>
     </html>
   );
 };
-
-  //  {/* <style></style> */}
-  //     {/* <Navbar /> */}
-  //     {/* <div className={outfit.className}>{children}</div> */}
-  //     {/* <Footer /> */}
 
